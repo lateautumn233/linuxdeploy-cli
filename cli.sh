6b5d0ae30678779240259319edec4bf1fbac1349
lateautumn233 <3487467850@qq.com>
2023-01-21 21:44:30 +0800

    Import and export rootfs supports zstd
    Signed-off-by: lateautumn233 <3487467850@qq.com>

--- cli.sh
+++ cli.sh
@@ -120,7 +120,7 @@ is_archive()
 {
     local src="$1"
     [ -n "${src}" ] || return 1
-    if [ -z "${src##*gz}" -o -z "${src##*bz2}" -o -z "${src##*xz}" ]; then
+    if [ -z "${src##*gz}" -o -z "${src##*bz2}" -o -z "${src##*xz}" -o -z "${src##*zst}" ]; then
         return 0
     fi
     return 1
@@ -787,7 +787,7 @@ rootfs_import()
     *tar)
         msg -n "Importing rootfs from tar archive ... "
         if [ -e "${rootfs_file}" ]; then
-            tar xf "${rootfs_file}" -C "${CHROOT_DIR}"
+            tar axf "${rootfs_file}" -C "${CHROOT_DIR}"
         elif [ -z "${rootfs_file##http*}" ]; then
             wget -q -O - "${rootfs_file}" | tar x -C "${CHROOT_DIR}"
         else
@@ -798,7 +798,7 @@ rootfs_import()
     *gz)
         msg -n "Importing rootfs from tar.gz archive ... "
         if [ -e "${rootfs_file}" ]; then
-            tar xzf "${rootfs_file}" -C "${CHROOT_DIR}"
+            tar axf "${rootfs_file}" -C "${CHROOT_DIR}"
         elif [ -z "${rootfs_file##http*}" ]; then
             wget -q -O - "${rootfs_file}" | tar xz -C "${CHROOT_DIR}"
         else
@@ -809,7 +809,7 @@ rootfs_import()
     *bz2)
         msg -n "Importing rootfs from tar.bz2 archive ... "
         if [ -e "${rootfs_file}" ]; then
-            tar xjf "${rootfs_file}" -C "${CHROOT_DIR}"
+            tar axf "${rootfs_file}" -C "${CHROOT_DIR}"
         elif [ -z "${rootfs_file##http*}" ]; then
             wget -q -O - "${rootfs_file}" | tar xj -C "${CHROOT_DIR}"
         else
@@ -820,7 +820,7 @@ rootfs_import()
     *xz)
         msg -n "Importing rootfs from tar.xz archive ... "
         if [ -e "${rootfs_file}" ]; then
-            tar xJf "${rootfs_file}" -C "${CHROOT_DIR}"
+            tar axf "${rootfs_file}" -C "${CHROOT_DIR}"
         elif [ -z "${rootfs_file##http*}" ]; then
             wget -q -O - "${rootfs_file}" | tar xJ -C "${CHROOT_DIR}"
         else
@@ -828,8 +828,19 @@ rootfs_import()
         fi
         is_ok "fail" "done" || return 1
     ;;
+    *zst)
+        msg -n "Importing rootfs from tar.zst archive ... "
+        if [ -e "${rootfs_file}" ]; then
+            tar axf "${rootfs_file}" -C "${CHROOT_DIR}"
+        elif [ -z "${rootfs_file##http*}" ]; then
+            wget -q -O - "${rootfs_file}" | tar "-I zstdmt" -C "${CHROOT_DIR}"
+        else
+            msg "fail"; return 1
+        fi
+        is_ok "fail" "done" || return 1
+    ;;
     *)
-        msg "Incorrect filename, supported only tar, tar.gz, tar.bz2 or tar.xz archives."
+        msg "Incorrect filename, supported only tar, tar.gz, tar.bz2 tar.xz or tar.zst archives."
         return 1
     ;;
     esac
@@ -846,21 +857,26 @@ rootfs_export()
     case "${rootfs_file}" in
     *gz)
         msg -n "Exporting rootfs as tar.gz archive ... "
-        tar czvf "${rootfs_file}" --exclude='./dev' --exclude='./sys' --exclude='./proc' -C "${CHROOT_DIR}" . >/dev/null
+        tar acf "${rootfs_file}" --exclude='./dev' --exclude='./sys' --exclude='./proc' -C "${CHROOT_DIR}" . >/dev/null
         is_ok "fail" "done" || return 1
     ;;
     *bz2)
         msg -n "Exporting rootfs as tar.bz2 archive ... "
-        tar cjvf "${rootfs_file}" --exclude='./dev' --exclude='./sys' --exclude='./proc' -C "${CHROOT_DIR}" . >/dev/null
+        tar acf "${rootfs_file}" --exclude='./dev' --exclude='./sys' --exclude='./proc' -C "${CHROOT_DIR}" . >/dev/null
         is_ok "fail" "done" || return 1
     ;;
     *xz)
         msg -n "Exporting rootfs as tar.xz archive ... "
-        tar cJvf "${rootfs_file}" --exclude='./dev' --exclude='./sys' --exclude='./proc' -C "${CHROOT_DIR}" . >/dev/null
+        tar acf "${rootfs_file}" --exclude='./dev' --exclude='./sys' --exclude='./proc' -C "${CHROOT_DIR}" . >/dev/null
+        is_ok "fail" "done" || return 1
+    ;;
+    *zst)
+        msg -n "Exporting rootfs as tar.zst archive ... "
+        tar acf "${rootfs_file}" --exclude='./dev' --exclude='./sys' --exclude='./proc' -C "${CHROOT_DIR}" . >/dev/null
         is_ok "fail" "done" || return 1
     ;;
     *)
-        msg "Incorrect filename, supported only gz, bz2 or xz archives."
+        msg "Incorrect filename, supported only gz, bz2 xz or zst archives."
         return 1
     ;;
     esac
@@ -956,8 +972,8 @@ COMMANDS:
       -i - install without configure
       -c - configure without install
       -n NAME - skip installation of this component
-   import FILE|URL - import a rootfs into the current container from archive (tgz, tbz2 or txz)
-   export FILE - export the current container as a rootfs archive (tgz, tbz2 or txz)
+   import FILE|URL - import a rootfs into the current container from archive (tgz, tbz2 txz or zst)
+   export FILE - export the current container as a rootfs archive (tgz, tbz2 txz or zst)
    shell [-u USER] [COMMAND] - execute the specified command in the container, by default /bin/bash
       -u USER - switch to the specified user
    mount - mount the container
